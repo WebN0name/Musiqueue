336e64352dc9029a09a147d404c50cab67dc63c9
KrimsN <deni.tim99@yandex.ru>
2020-12-06 01:05:03 +0700

    ВРоде работает commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
       <Router>
         <Switch>
           <Route exact path = '/' component = {Home}/>
-          <Route path = '/auth' component = {Auth}/>
+          {/*<Route path = '/auth/' component = {Auth}/>*/}
+            <Route
+                path='/auth'
+                render={(props) => (
+                    <Auth {...props} />
+                )}
+            />
         </Switch>
       </Router>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import {useEffect} from 'react'
 
 export default function Home(){
     useEffect(() =>{
-        window.location.replace('https://oauth.vk.com/authorize?client_id=7689075&display=page&redirect_uri=https://localhost:3000/auth&scope=friends&response_type=token&v=5.52')  
+        window.location.replace('https://oauth.vk.com/authorize?client_id=7689075&display=page&redirect_uri=https://localhost:3000/auth&scope=audio&response_type=token&v=5.52')
       }, [])
 
     return(
