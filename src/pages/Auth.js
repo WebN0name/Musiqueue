b366e1260bb98e3a38d9a125b03132f91b87125a
WebN0name <HaswelVc@yandex.ru>
2020-12-05 23:54:31 +0700

    first commit

---
+++ src/pages/Auth.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+export default function Auth(){
+
+    useEffect(() =>{
+        // let path = location.search
+        // console.log(path)
+    },[])
+
+    return(
+        <>
+            <h1>хуй</h1>
+        </>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import {useEffect} from 'react'
+
+
+export default function Home(){
+    useEffect(() =>{
+        window.location.replace('https://oauth.vk.com/authorize?client_id=7689075&display=page&redirect_uri=https://localhost:3000/auth&scope=friends&response_type=token&v=5.52')  
+      }, [])
+
+    return(
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
